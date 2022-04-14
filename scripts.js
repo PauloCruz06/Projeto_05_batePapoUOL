a9bf0f12fabc6a6405c7258469183ace19d68b40
Paulo <pc5783346@gmail.com>
2022-04-14 14:05:02 -0300

    implementação do layout e função de mensagem

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
+const messageDefault = {time: "(12:22:50)", name: "Usuário", message: "Olá, como vai? Essa é uma mensagem padrão", receiver: "Todos"};
 
+function sendMessage(){
+    const box = document.querySelector(".conteiner");
+    box.innerHTML += `<div class="message"><p><light>${messageDefault.time}</light> <b>${messageDefault.name}</b> para <b>${messageDefault.receiver}</b>: ${messageDefault.message}</p></div>` 
+}
 
 function openMenu(){
     const menu = document.querySelector(".participants");
