e6ce7c8c55714450616d5751c5d1981029956361
Paulo <pc5783346@gmail.com>
2022-04-16 11:26:37 -0300

    inplementação do envio de mensagens para os servidor

--- scripts.js
+++ scripts.js
@@ -1,5 +1,4 @@
-const messageDefault = {time: "(12:22:50)", name: "Usuário", message: "Olá, como vai? Essa é uma mensagem padrão", receiver: "Todos"};
-let user = {name: ""};
+let user = {name: "", from: "", to: "", text: "", type: "", time: ""};
 login();
 
 //funçôes de login
@@ -12,10 +11,14 @@ function login (){
 
 function userLogin(){
     user = {name: prompt("Digite o seu nome")};
+    user.from = user.name;
+    selectcontact(document.querySelector(".contact p"));
+    user.type = "message";
+    //selectvisibility(document.querySelector(".visibility p"));
     const promiseLogin = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user);
-    promiseLogin.then(setInterval(getParticipants, 5000));
+    promiseLogin.then(setInterval(getParticipants, 10000));
     promiseLogin.then(setInterval(userStatus, 4000));
-    promiseLogin.then(setInterval(getMessages, 2000));
+    promiseLogin.then(setInterval(getMessages, 3000));
     promiseLogin.catch(userError);
 }
 
@@ -27,10 +30,9 @@ function getParticipants(){
 
 function setParticipants(part){
     //console.log(part.data);
-    document.querySelector(".menu :nth-child(2)").innerHTML = "";
-    document.querySelector(".menu :nth-child(2)").innerHTML = `<div class="select contact"><ion-icon name="people"></ion-icon><p>Todos<ion-icon name="checkmark-sharp"></ion-icon></p></div>`
+    document.querySelector(".menu :nth-child(2)").innerHTML = `<div class="select contact"><ion-icon name="people"></ion-icon><p onclick="selectcontact(this)" name="Todos">Todos<ion-icon class="check" name="checkmark-sharp"></ion-icon></p></div>`
     for(let i=0; i<part.data.length; i++){
-        document.querySelector(".menu :nth-child(2)").innerHTML += `<div class="select contact"><ion-icon name="people"></ion-icon><p>${part.data[i].name}<ion-icon class="hidden" name="checkmark-sharp"></ion-icon></p></div>`
+        document.querySelector(".menu :nth-child(2)").innerHTML += `<div class="select contact"><ion-icon name="people"></ion-icon><p onclick="selectcontact(this)" name="${part.data[i].name}">${part.data[i].name}<ion-icon class="hidden" name="checkmark-sharp"></ion-icon></p></div>`
     }
 }
 
@@ -51,7 +53,7 @@ function getMessages(){
 function setMessages(mess){
     //console.log(mess.data);
     const arrmax = mess.data.length;
-    let cont = 30;
+    let cont = 35;
     let j = arrmax;
     document.querySelector(".conteiner").innerHTML = "";
     while(cont >= 0){
@@ -64,10 +66,12 @@ function setMessages(mess){
         }else if(mess.data[i].type === "status"){
             document.querySelector(".conteiner").innerHTML += `<div class="message type-status"><p><light>(${mess.data[i].time})</light> <b>${mess.data[i].from}</b> ${mess.data[i].text}<p></div>`;
         }else{
-            ocument.querySelector(".conteiner").innerHTML += `<div class="message type-private"><p><light>(${mess.data[i].time})</light> <b>${mess.data[i].from}</b> para <b>${mess.data[i].to}</b>: ${mess.data[i].text}<p></div>`;
+            document.querySelector(".conteiner").innerHTML += `<div class="message type-private"><p><light>(${mess.data[i].time})</light> <b>${mess.data[i].from}</b> para <b>${mess.data[i].to}</b>: ${mess.data[i].text}<p></div>`;
         }
         
     }
+    user.time = mess.data[arrmax-1].time;
+    document.querySelector (".conteiner > .message:last-child").scrollIntoView();
 }
 
 
@@ -95,8 +99,14 @@ function userError(error){
 //funcionalidades do layout
 
 function sendMessage(){
+    getinput(document.querySelector("input"));
+    const promiseSend = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", user);
     const box = document.querySelector(".conteiner");
-    box.innerHTML += `<div class="message"><p><light>${messageDefault.time}</light> <b>${messageDefault.name}</b> para <b>${messageDefault.receiver}</b>: ${messageDefault.message}</p></div>` 
+    if(user.type === "message"){
+        box.innerHTML += `<div class="message"><p><light>${user.time}</light> <b>${user.from}</b> para <b>${user.to}</b>: ${user.text}</p></div>`
+    } else{
+        box.innerHTML += `<div class="message type-private"><p><light>${user.time}</light> <b>${user.from}</b> para <b>${user.to}</b>: ${user.text}</p></div>`
+    }
 }
 
 function openMenu(){
@@ -108,3 +118,41 @@ function openMenu(){
     }
     //document.querySelector(".contact p ion-icon").classList.toggle("hidden");
 }
+
+function getinput(input){
+    user.text = input.value;
+    input.value = "";
+    if(user.text === ""){
+        alert("Digite alguma coisa!");
+    }
+    document.querySelector(".input-box :last-child").classList.remove("hidden");
+    if(user.type === "message"){
+        document.querySelector(".subtittle").innerHTML = `Enviando para ${user.to} (publicamente)`
+    } 
+}
+
+function selectcontact(contact){
+    if(contact.lastChild.getAttribute("class") === "hidden md hydrated"){
+        let uncheck = document.querySelector(".contact .check");
+        uncheck.classList.remove("check");
+        uncheck.classList.add("hidden");
+        contact.lastChild.classList.remove("hidden");
+        contact.lastChild.classList.add("check");
+        user.to = contact.getAttribute("name");
+    }else{
+        user.to = "Todos";
+    }
+}
+
+/*function selectvisibility(visibility){
+    if(visibility.lastChild.getAttribute("class") === "hidden md hydrated"){
+        let uncheck = document.querySelector(".visibility .check");
+        uncheck.classList.remove("check");
+        uncheck.classList.add("hidden");
+        visibility.lastChild.classList.remove("hidden");
+        visibility.lastChild.classList.add("check");
+        user.to = visibility.getAttribute("name");
+    }else{
+        user.to = "message";
+    }
+}*/
