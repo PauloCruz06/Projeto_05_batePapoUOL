e37c8f77ce42b588d4e2b76c1d821c04da0f37fb
Paulo <pc5783346@gmail.com>
2022-04-16 00:59:09 -0300

    add do login na sala perguntando o nome por prompt

--- scripts.js
+++ scripts.js
@@ -1,4 +1,51 @@
 const messageDefault = {time: "(12:22:50)", name: "Usuário", message: "Olá, como vai? Essa é uma mensagem padrão", receiver: "Todos"};
+let user = {time: "", name: "", message: "Olá", receiver: "Todos"};
+login();
+
+//funçôes de login
+
+function login (){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    promise.then(console.log(promise));
+    promise.then(userLogin);
+    promise.catch(serverError);
+}
+
+function userLogin(){
+    user = {name: prompt("Digite o seu nome")};
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user);
+    promise.then(verparticipantes);
+    promise.catch(userError);
+}
+
+function verparticipantes(){
+    const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/participants");
+    promise.then(console.log(promise));
+}
+
+function serverError(error){
+    if(error.response.status === 500){
+        alert("Não foi possível conectar, o servidor não respondeu a solicitação");
+    }
+    return;
+}
+
+function userError(error){
+    if(error.response.status === 400){
+        alert("Não foi possível reconhecer este usuário");
+        userLogin();
+    }else if(error.response.status === 409){
+        alert("O nome de usuário já está sendo usado na sala");
+        userLogin();
+    }else if(error.response.status === 422){
+        alert("Nome de usuário inválido");
+        userLogin();
+    }
+    return;
+}
+
+
+//funcionalidades do layout
 
 function sendMessage(){
     const box = document.querySelector(".conteiner");
